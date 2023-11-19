e61a697d1f0683893722f253e4928cc83b28e747
Berkin KOCA <bekoca@innova.com.tr>
2023-11-19 19:32:33 +0300

    Section titles added

--- NetflixClone ProgrammaticUI/Controllers/HomeViewController.swift
+++ NetflixClone ProgrammaticUI/Controllers/HomeViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class HomeViewController: UIViewController {
      
+     let sectionTitles: [String] = [
+          "Trending Movies",
+          "Popular",
+          "Trending TV Series",
+          "Upcoming Movies",
+          "Top Rated"
+     ]
+     
      private let homeFeedTable: UITableView = {
           let table = UITableView(frame: .zero, style: .grouped)
           table.register(CollectionViewTableViewCell.self, forCellReuseIdentifier: CollectionViewTableViewCell.identifier)
@@ -56,7 +64,7 @@ class HomeViewController: UIViewController {
 extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
      
      func numberOfSections(in tableView: UITableView) -> Int {
-          return 20
+          return sectionTitles.count
      }
      
      
@@ -82,11 +90,24 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
           return 40
      }
      
+     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+          return sectionTitles[section]
+     }
+     
+     func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+          guard let header = view as? UITableViewHeaderFooterView else { return }
+          header.textLabel?.font = .systemFont(ofSize: 18, weight: .semibold)
+          header.textLabel?.frame = CGRect(x: header.bounds.origin.x + 20, y: header.bounds.origin.y, width: 100, height: header.bounds.height)
+          header.textLabel?.textColor = .white
+          
+          
+     }
+     
      func scrollViewDidScroll(_ scrollView: UIScrollView) {
           let defaultOffset = view.safeAreaInsets.top
           let offset = scrollView.contentOffset.y + defaultOffset
           
-          //When you Scroll down navbar will disapper
+          //When you Scroll down navbar will disappear
           //When you Scroll up navbar won't slide
           navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
      }
