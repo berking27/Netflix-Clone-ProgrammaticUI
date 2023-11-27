e5b6fc8fb6928849bba37c1750dadced411a2c4f
Berkin KOCA <bekoca@innova.com.tr>
2023-11-27 17:36:22 +0300

    Randomly fetching HeroHeaderView poster and displaying

--- NetflixClone ProgrammaticUI/Controllers/Core/HomeViewController.swift
+++ NetflixClone ProgrammaticUI/Controllers/Core/HomeViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class HomeViewController: UIViewController {
      
+     private var randomTrendingMovie: Title?
+     private var headerView: HeroHeaderUIView?
+     
      let sectionTitles: [String] = [
           "Trending Movies",
           "Trending TV Series",
@@ -36,14 +39,30 @@ class HomeViewController: UIViewController {
           homeFeedTable.dataSource = self
           
           
-          let headerView = HeroHeaderUIView(frame: CGRect(x: 0,
-                                                          y: 0,
-                                                          width: view.bounds.width,
-                                                          height: 450))
+          headerView = HeroHeaderUIView(frame: CGRect(x: 0,
+                                                      y: 0,
+                                                      width: view.bounds.width,
+                                                      height: 450))
           homeFeedTable.tableHeaderView = headerView
           
+          configureHeroHeaderView()
           
           
+     }
+     
+     private func configureHeroHeaderView() {
+          
+          APICaller.shared.getTrendingTvs { [weak self] result in
+               switch result {
+               case .success(let titles):
+                    let selectedTitle = titles.randomElement()
+                    self?.randomTrendingMovie = selectedTitle
+                    self?.headerView?.configure(with: TitleViewModel(titleName: selectedTitle?.original_title ?? "", posterURL: selectedTitle?.poster_path ?? ""))
+                    
+               case .failure(let error):
+                    print(error.localizedDescription)
+               }
+          }
      }
      
      private func configureNavbar() {
@@ -188,7 +207,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
 extension HomeViewController: CollectionViewTableViewCellDelegate {
      
      func CollectionViewTableViewCellDidTapCell(_ cell: CollectionViewTableViewCell, viewModel: TitlePreviewViewModel) {
-          DispatchQueue.main.async { [weak self] in 
+          DispatchQueue.main.async { [weak self] in
                let vc = TitlePreviewViewController()
                vc.configure(with: viewModel)
                self?.navigationController?.pushViewController(vc, animated: true)
@@ -198,5 +217,5 @@ extension HomeViewController: CollectionViewTableViewCellDelegate {
 
 #Preview(){
      HomeViewController()
-// MainTabBarViewController()
+     // MainTabBarViewController()
 }
