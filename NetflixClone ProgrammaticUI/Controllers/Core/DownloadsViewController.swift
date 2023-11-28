729276ace57f1701accb211e23300da87d462439
berking27 <berkin.koca01@gmail.com>
2023-11-29 00:25:33 +0300

    Core Data Added to project, Also Downloaded Title stored in core data, And DownloadsVC designed and developed.

--- NetflixClone ProgrammaticUI/Controllers/Core/DownloadsViewController.swift
+++ NetflixClone ProgrammaticUI/Controllers/Core/DownloadsViewController.swift
@@ -8,12 +8,121 @@
 import UIKit
 
 class DownloadsViewController: UIViewController {
+     
+     private var titles: [TitleItem] = [TitleItem]()
+     
+     private let downloadTable: UITableView = {
+          
+          let table = UITableView()
+          
+          table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+          
+          return table
+     }()
+     
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          // Do any additional setup after loading the view.
+          view.backgroundColor = .systemBackground
+          title = "Downloads"
+          view.addSubview(downloadTable)
+          navigationController?.navigationBar.prefersLargeTitles = true
+          navigationController?.navigationItem.largeTitleDisplayMode = .always
+          
+          downloadTable.delegate = self
+          downloadTable.dataSource = self
+          
+          fetchLocalStorage()
+          NotificationCenter.default.addObserver(forName: NSNotification.Name("Downloaded"), object: nil, queue: nil) { _ in
+               self.fetchLocalStorage()
+          }
+     }
+     
+     private func fetchLocalStorage() {
+          DataPersistenceManager.shared.fetchingTitlesFromDatabase { [weak self] result in
+               switch result {
+               case .success(let titles):
+                    self?.titles = titles
+                    DispatchQueue.main.async{
+                         self?.downloadTable.reloadData()
+                    }
+               case .failure(let error):
+                    print(error.localizedDescription)
+               }
+          }
+     }
+     
+     override func viewDidLayoutSubviews() {
+          super.viewDidLayoutSubviews()
+          downloadTable.frame = view.bounds
+     }
+     
+     
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-         view.backgroundColor = .systemBackground
-    }
+extension DownloadsViewController: UITableViewDataSource, UITableViewDelegate {
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+          return titles.count
+     }
+     
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+          guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else {
+               return UITableViewCell()
+          }
+          
+          let title = titles[indexPath.row]
+          cell.configure(with: TitleViewModel(titleName: title.original_title ?? title.original_name ?? "Unknown", posterURL: title.poster_path ?? ""))
+          return cell
+     }
+     
+     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+          return 180
+     }
+     
+     
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+          tableView.deselectRow(at: indexPath, animated: true)
+          
+          let title = titles[indexPath.row]
+          
+          guard let titleName = title.original_name ?? title.original_title else {
+               return
+          }
+          
+          APICaller.shared.getMovie(with: titleName) { [weak self] result in
+               switch result {
+               case .success(let videoElement):
+                    DispatchQueue.main.async {
+                         let vc = TitlePreviewViewController()
+                         vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeVideo: videoElement, titleOverView: title.overview ?? "Unknown overview"))
+                         self?.navigationController?.pushViewController(vc, animated: true)
+                    }
+                    
+               case .failure(let error):
+                    print(error.localizedDescription)
+               }
+          }
+     }
+     
+     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+          switch editingStyle {
+          case .delete:
+               DataPersistenceManager.shared.deleteTitleWith(model: titles[indexPath.row]) { [weak self] result in
+                    switch result {
+                    case .success():
+                         print("Deleted")
+                    case .failure(let error):
+                         print(error.localizedDescription)
+                    }
+                    self?.titles.remove(at: indexPath.row)
+                    tableView.deleteRows(at: [indexPath], with: .fade)
+                    
+               }
+          default:
+               break;
+          }
+     }
+     
 }
 
 #Preview(){
---
+++ NetflixClone ProgrammaticUI/Managers/DataPersistenceManager.swift
@@ -0,0 +1,84 @@
+//
+//  DataPersistenceManager.swift
+//  NetflixClone ProgrammaticUI
+//
+//  Created by Berkin Koca on 28.11.2023.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class DataPersistenceManager {
+     
+     enum DatabaseError: Error {
+          case  failedToSaveData
+          case  failedToFetchData
+          case  failedToDeleteData
+          
+     }
+     
+     static let shared = DataPersistenceManager()
+     
+     private init(){}
+     
+     func downloadTitleWith(model: Title, completion: @escaping (Result<Void, Error>) -> Void) {
+          
+          guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {return}
+          
+          let context = appDelegate.persistentContainer.viewContext
+          
+          let item = TitleItem(context: context)
+          
+          item.id = Int64(model.id)
+          item.original_title = model.original_title
+          item.original_name = model.original_name
+          item.media_type = model.media_type
+          item.overview = model.overview
+          item.release_date = model.release_date
+          item.vote_count = Int64(model.vote_count)
+          item.vote_average = model.vote_average
+          item.poster_path = model.poster_path
+          
+          do {
+               try context.save()
+               completion(.success(())) //instead of writing Void just write ()
+          } catch {
+               completion(.failure(DatabaseError.failedToSaveData))
+          }
+          
+     }
+     
+     func fetchingTitlesFromDatabase(completion: @escaping (Result<[TitleItem], Error>) -> Void) {
+          
+          guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+          
+          let context = appDelegate.persistentContainer.viewContext
+          
+          let request: NSFetchRequest<TitleItem>
+          request = TitleItem.fetchRequest()
+          
+          do {
+               let titles = try context.fetch(request)
+               completion(.success(titles))
+          } catch {
+               completion(.failure(DatabaseError.failedToFetchData))
+          }
+     }
+     
+     func deleteTitleWith(model: TitleItem, completion: @escaping (Result<Void, Error>) -> Void) {
+          
+          guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+          
+          let context = appDelegate.persistentContainer.viewContext
+          
+          context.delete(model)
+          
+          do {
+               try context.save()
+               completion(.success(()))
+          } catch {
+               completion(.failure(DatabaseError.failedToDeleteData))
+          }
+     }
+}
