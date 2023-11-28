4dc6845d00aca0d0e71cf99f37b1e1d89c478b34
Berkin KOCA <bekoca@innova.com.tr>
2023-11-28 19:35:30 +0300

    UpcomingVC, SearchVC and SearchResultVC make clickable and opens TitlePreviewVC

--- NetflixClone ProgrammaticUI/Controllers/Core/UpComingViewController.swift
+++ NetflixClone ProgrammaticUI/Controllers/Core/UpComingViewController.swift
@@ -10,29 +10,29 @@ import UIKit
 class UpComingViewController: UIViewController {
      
      private var titles: [Title] = [Title]()
-
+     
      private let upcomingTable: UITableView = {
-        
+          
           let table = UITableView()
           table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
           return table
      }()
      
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-         view.backgroundColor = .systemBackground
-         title = "Upcoming"
-         navigationController?.navigationBar.prefersLargeTitles = true
-         navigationController?.navigationItem.largeTitleDisplayMode = .always
-         
-         view.addSubview(upcomingTable)
-         upcomingTable.delegate = self
-         upcomingTable.dataSource = self
-         
-         fetchUpcoming()
-    }
+     override func viewDidLoad() {
+          super.viewDidLoad()
+          
+          // Do any additional setup after loading the view.
+          view.backgroundColor = .systemBackground
+          title = "Upcoming"
+          navigationController?.navigationBar.prefersLargeTitles = true
+          navigationController?.navigationItem.largeTitleDisplayMode = .always
+          
+          view.addSubview(upcomingTable)
+          upcomingTable.delegate = self
+          upcomingTable.dataSource = self
+          
+          fetchUpcoming()
+     }
      
      override func viewDidLayoutSubviews() {
           super.viewDidLayoutSubviews()
@@ -73,6 +73,30 @@ extension UpComingViewController: UITableViewDelegate, UITableViewDataSource {
      
      func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
           return 180
+     }
+     
+     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+          tableView.deselectRow(at: indexPath, animated: true)
+          
+          let title = titles[indexPath.row]
+          
+          guard let titleName = title.original_name ?? title.original_title else {
+               return
+          }
+          
+          APICaller.shared.getMovie(with: titleName) { [weak self] result in
+               switch result {
+               case .success(let videoElement):
+                    DispatchQueue.main.async {
+                         let vc = TitlePreviewViewController()
+                         vc.configure(with: TitlePreviewViewModel(title: titleName, youtubeVideo: videoElement, titleOverView: title.overview ?? "Unknown overview"))
+                         self?.navigationController?.pushViewController(vc, animated: true)
+                    }
+                    
+               case .failure(let error):
+                    print(error.localizedDescription)
+               }
+          }
      }
      
 }
--- NetflixClone ProgrammaticUI/Controllers/General/SearchResultsViewController.swift
+++ NetflixClone ProgrammaticUI/Controllers/General/SearchResultsViewController.swift
@@ -7,9 +7,15 @@
 
 import UIKit
 
+protocol SearchResultsViewControllerDelegate: AnyObject {
+     func searchResultsViewControllerDidTapItem(_ viewModel: TitlePreviewViewModel)
+}
+
 class SearchResultsViewController: UIViewController {
      
      public var titles: [Title] = [Title]()
+     
+     public weak var delegate: SearchResultsViewControllerDelegate?
      
      public let searchResultsCollectionView: UICollectionView = {
           
@@ -57,6 +63,23 @@ extension SearchResultsViewController: UICollectionViewDelegate, UICollectionVie
           
      }
      
+     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+          collectionView.deselectItem(at: indexPath, animated: true)
+          
+          let title = titles[indexPath.row]
+          let titleName = title.original_title ?? ""
+          
+          APICaller.shared.getMovie(with: titleName) { [weak self] result in
+               switch result {
+               case .success(let videoElement):
+                    self?.delegate?.searchResultsViewControllerDidTapItem(TitlePreviewViewModel(title: title.original_name ?? title.original_title ?? "" , youtubeVideo: videoElement ,titleOverView: title.overview ?? ""))
+               case .failure(let error):
+                    print(error.localizedDescription)
+               }
+          }
+          
+
+     }
      
 }
 
